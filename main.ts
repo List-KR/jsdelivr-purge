0ae49a386ecdeda9ae5990adc64ad46a4f712362
PiQuark6046 <piquark6046@proton.me>
2023-03-16 18:21:06 +0000

    Check if selected branches are valid

--- main.ts
+++ main.ts
@@ -5,13 +5,21 @@ import * as Threads from 'worker_threads'
 const Octokit = new GitHub.Octokit({ auth: process.env['GITHUB_TOKEN'] })
 const RepoName = process.env['GITHUB_REPO'].split('/')[1]
 const RepoOwner = process.env['GITHUB_REPO'].split('/')[0]
+const KnownBranches = await Octokit.rest.repos.listBranches({ owner: RepoOwner, repo: RepoName, page: Number.MAX_SAFE_INTEGER, per_page: 100 })
+  .then((Result) => { return Result.data.map((Data) => { return Data.name }) })
 var Branches = Actions.getMultilineInput('branches', { required: true })
 var BrancheThreads:Threads.Worker[] = []
 
-// Check if an user selects all branches.
+// Check if an user selects all branches and selected branches are valid.
 if (Branches.length === 1 && Branches[0] === '**') {
-  Branches = await Octokit.rest.repos.listBranches({ owner: RepoOwner, repo: RepoName, page: Number.MAX_SAFE_INTEGER, per_page: 100 })
-    .then((Result) => { return Result.data.map((Data) => { return Data.name }) })
+  Branches = KnownBranches
+} else {
+  Branches.forEach((element) => {
+    if (KnownBranches.some((KnownBranche) => { return KnownBranche !== element })) {
+      Branches = Branches.filter((Branch) => { return Branch !== element })
+      Actions.warning(`The ${element} branch does not exist.`)
+    }
+  })
 }
 
 Actions.info(`The following branches will be processed:
