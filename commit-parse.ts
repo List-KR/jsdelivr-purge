7260bea137732bf84fd2a26ecb29275a4b87976e
PiQuark6046 <piquark6046@proton.me>
2023-03-26 18:33:52 +0000

    Update indentation

--- commit-parse.ts
+++ commit-parse.ts
@@ -7,23 +7,23 @@ const Octokit = new GitHub.Octokit({ auth: process.env['GITHUB_TOKEN'] })
 const [RepoOwner, RepoName] = process.env['GITHUB_REPO'].split('/')
 
 interface CommitData {
-	path?: string
-	mode?: string
-	type?: string
-	sha?: string
-	size?: Number
-	url?: string
+  path?: string
+  mode?: string
+  type?: string
+  sha?: string
+  size?: Number
+  url?: string
 }
 
 export async function Parse(CommitData:CommitData[], DirectoryPrefix:string, Branch:string) {
-	var ChangedFiles:string[] = []
-	for (const Tree of CommitData) {
-		Actions.info(`Thread for ${Branch}: Looking up with ${Tree.sha}: ${DirectoryPrefix ?? ''}${Tree.path}`)
-		if (Tree.type === 'blob') ChangedFiles.push(`${DirectoryPrefix ?? ''}${Tree.path}`)
-		if (Tree.type === 'tree') {
-			ChangedFiles = ChangedFiles.concat(await Parse(await Octokit.rest.git.getTree(
-				{ owner: RepoOwner, repo: RepoOwner, tree_sha: Tree.sha }).then((Data) => { return Data.data.tree }), `${DirectoryPrefix ?? ''}${Tree.path}/`, Branch))
-		}
-	}
-	return ChangedFiles
+  var ChangedFiles:string[] = []
+  for (const Tree of CommitData) {
+    Actions.info(`Thread for ${Branch}: Looking up with ${Tree.sha}: ${DirectoryPrefix ?? ''}${Tree.path}`)
+    if (Tree.type === 'blob') ChangedFiles.push(`${DirectoryPrefix ?? ''}${Tree.path}`)
+    if (Tree.type === 'tree') {
+      ChangedFiles = ChangedFiles.concat(await Parse(await Octokit.rest.git.getTree(
+        { owner: RepoOwner, repo: RepoOwner, tree_sha: Tree.sha }).then((Data) => { return Data.data.tree }), `${DirectoryPrefix ?? ''}${Tree.path}/`, Branch))
+    }
+  }
+  return ChangedFiles
 }
