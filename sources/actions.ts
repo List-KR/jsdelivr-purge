b345896850a632f5a23b594ff41dc22ce3399bab
piquark6046 <piquark6046@proton.me>
2024-03-11 08:21:10 +0000

    style: better expression processing GitHub workflow runs

--- sources/actions.ts
+++ sources/actions.ts
@@ -2,6 +2,17 @@ import * as GitHub from '@octokit/rest'
 import {DateTime} from 'luxon'
 import type {ProgramOptionsType} from './types.js'
 
+async function ListWorkflowRuns(ProgramOptions: ProgramOptionsType, EventType: string) {
+	const GitHubInstance = new GitHub.Octokit({auth: ProgramOptions.ghToken})
+	const [RepoOwner, RepoName] = ProgramOptions.repo.split('/')
+	const WorkflowRuns = await GitHubInstance.actions.listWorkflowRuns({
+		event: EventType,
+		owner: RepoOwner, repo: RepoName,
+		workflow_id: /(?<=^[A-Za-z0-9-_.]+\/[A-Za-z0-9-_.]+\/\.github\/workflows\/).+\.yml(?=@refs\/)/.exec(ProgramOptions.workflowRef)[0],
+	}).then(WorkflowRuns => WorkflowRuns.data.workflow_runs)
+	return WorkflowRuns
+}
+
 /**
  * @name GetLatestWorkflowTime
  * @description Get the latest workflow time.
@@ -9,21 +20,18 @@ import type {ProgramOptionsType} from './types.js'
  * @returns {Promise<number>} The latest workflow time in milliseconds.
  */
 export async function GetLatestWorkflowTime(ProgramOptions: ProgramOptionsType): Promise<number> {
-	const GitHubInstance = new GitHub.Octokit({auth: ProgramOptions.ghToken})
-	const [RepoOwner, RepoName] = ProgramOptions.repo.split('/')
 	var LatestWorkflowRunTime = 0
-	const WorkflowRuns = await GitHubInstance.actions.listWorkflowRuns({
-		event: 'push',
-		owner: RepoOwner, repo: RepoName,
-		workflow_id: /(?<=^[A-Za-z0-9-_.]+\/[A-Za-z0-9-_.]+\/\.github\/workflows\/).+\.yml(?=@refs\/)/.exec(ProgramOptions.workflowRef)[0],
-	}).then(WorkflowRuns => WorkflowRuns.data.workflow_runs)
-	WorkflowRuns.push(...(await GitHubInstance.actions.listWorkflowRuns({
-		event: 'release',
-		owner: RepoOwner, repo: RepoName,
-		workflow_id: /(?<=^[A-Za-z0-9-_.]+\/[A-Za-z0-9-_.]+\/\.github\/workflows\/).+\.yml(?=@refs\/)/.exec(ProgramOptions.workflowRef)[0],
-	}).then(WorkflowRuns => WorkflowRuns.data.workflow_runs)))
+	let WorkflowRuns: ReturnType<typeof ListWorkflowRuns> = null
+	for (const EventType of ['push', 'release']) {
+		if (WorkflowRuns === null) {
+			WorkflowRuns = ListWorkflowRuns(ProgramOptions, EventType)
+		} else {
+			// eslint-disable-next-line no-await-in-loop
+			(await WorkflowRuns).push(...await ListWorkflowRuns(ProgramOptions, EventType))
+		}
+	}
 
-	for (const WorkflowRun of WorkflowRuns) {
+	for (const WorkflowRun of await WorkflowRuns) {
 		if (WorkflowRun.status === 'completed' && WorkflowRun.conclusion === 'success'
 		&& DateTime.fromISO(WorkflowRun.updated_at).toMillis() > LatestWorkflowRunTime) {
 			LatestWorkflowRunTime = DateTime.fromISO(WorkflowRun.updated_at).toMillis()
